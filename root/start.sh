cc4171577789987b6fcb88244579d4b1587f85ab
PJ Linebaugh <pjl@renci.org>
2023-10-17 19:04:02 -0400

    set NB_PREFIX to '' and add nginx location for auth-sign-in

--- root/start.sh
+++ root/start.sh
@@ -8,7 +8,8 @@ export USER_GID=${USER_GID-"0"}
 export DEFAULT_USER="helx"
 # Use NB_PREFIX for base path if it is set and BASE_PATH is not set.
 # NB_PREFIX will be set if launched from HeLx.
-export NB_PREFIX=${NB_PREFIX-"/"}
+# export NB_PREFIX=${NB_PREFIX-""}
+export NB_PREFIX=""
 # if BASE_PATH is set then use that and override NB_PREFIX if set.
 export BASE_PATH=${BASE_PATH-$NB_PREFIX}
 
