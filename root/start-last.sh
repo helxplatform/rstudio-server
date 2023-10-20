684c192de6012378c4964ea7acb18db4de8b3f33
PJ Linebaugh <pjl@renci.org>
2023-10-20 17:26:14 -0400

    remove unneeded if statement that checks for BASE_PATH

--- root/start-last.sh
+++ root/start-last.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-if [ "$BASE_PATH" != "" ]; then
-  echo "www-root-path=$BASE_PATH/">>/etc/rstudio/rserver.conf
-fi
-
 # Modify username that rstudio-server runs as.
 cp /etc/rstudio/rserver.conf /tmp/rserver.conf
 sed -i -e "s/^server-user=rstudio/server-user=$USER/" /tmp/rserver.conf
