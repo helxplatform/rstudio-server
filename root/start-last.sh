7cd29d972d0278f7944aa516077399e590e404c3
PJ Linebaugh <pjl@renci.org>
2023-10-25 13:38:01 -0400

    works to use nginx to proxy rstudio-server locally with docker-compose and in a deployment yaml to k8s

--- root/start-last.sh
+++ root/start-last.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-if [ "$BASE_PATH" != "" ]; then
-  echo "www-root-path=$BASE_PATH/">>/etc/rstudio/rserver.conf
-fi
-
 # Modify username that rstudio-server runs as.
 cp /etc/rstudio/rserver.conf /tmp/rserver.conf
 sed -i -e "s/^server-user=rstudio/server-user=$USER/" /tmp/rserver.conf
+if [ "$RSTUDIO_SERVER_BASE_PATH" != "" ]; then
+  # Remove any existing lines for www-root-path (usually will not be there).
+  # Rstudio will assume the root path '/' if not defined.
+  sed -i -e "s/^www-root-path.*//" /tmp/rserver.conf
+  # Add line to the end of rserver.conf.
+  echo "www-root-path=$RSTUDIO_SERVER_BASE_PATH">>/tmp/rserver.conf
+fi
+# Remove any existing lines for log-level (usually will not be there).
+sed -i -e "s/^log-level.*//" /tmp/rserver.conf
+echo "log-level=${RSTUDIO_LOG_LEVEL}">>/etc/rstudio/logging.conf
 cp /tmp/rserver.conf /etc/rstudio/rserver.conf
 rm /tmp/rserver.conf
 
