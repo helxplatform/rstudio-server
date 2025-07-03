b129e5a20382474ce7083d3e6bdff130c98746c0
frostyfan109 <groupe@renci.org>
2025-07-03 12:39:19 -0400

    focal build

--- create-builder-image.sh
+++ create-builder-image.sh
@@ -32,7 +32,7 @@ else
 fi
 
 REPO="containers.renci.org/helxplatform/rstudio-base"
-IMAGE_TAG="jammy-amd64-builder-$RSTUDIO_SOURCE_TAG_DASHES"
+IMAGE_TAG="focal-amd64-builder-$RSTUDIO_SOURCE_TAG_DASHES"
 
 # check to see if there's already a built image
 IMAGEID=$(docker images "$REPO:$IMAGE_TAG" --format "{{.ID}}")
@@ -43,7 +43,7 @@ else
 fi
 
 cd $RSTUDIO_SRC_DIR
-IMAGE=jammy
+IMAGE=focal
 docker build                              \
   --tag "$REPO:$IMAGE_TAG"                  \
   --file "docker/jenkins/Dockerfile.$IMAGE" \
